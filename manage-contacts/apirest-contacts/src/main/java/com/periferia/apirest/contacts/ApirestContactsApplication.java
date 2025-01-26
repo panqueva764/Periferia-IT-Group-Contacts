7ebd230cfda526ff8a0104718fb50bd35e1dc7df
panqueva764 <panqueva764@gmail.com>
2025-01-26 12:40:23 -0500

    implementa inicio del proyecto

---
+++ manage-contacts/apirest-contacts/src/main/java/com/periferia/apirest/contacts/ApirestContactsApplication.java
@@ -0,0 +1,13 @@
+package com.periferia.apirest.contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestContactsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestContactsApplication.class, args);
+	}
+
+}
---
+++ manage-contacts/apirest-contacts/src/test/java/com/periferia/apirest/contacts/ApirestContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.periferia.apirest.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
